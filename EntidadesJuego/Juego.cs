23f7a15cb612556145db59a63888f3c494849963
Juan Aira <juanaira_01@hotmail.com>
2018-04-24 23:13:21 -0300

    Ranking

--- EntidadesJuego/Juego.cs
+++ EntidadesJuego/Juego.cs
@@ -38,16 +38,62 @@ namespace EntidadesJuego
             return nuevaPartida;
         }
 
-        //private void ActualizarRanking()
-        //{
-        //    var ListaRetornar = new List<Ranking>();
+        public void ActualizarRanking()
+        {
+            var ListaRetornar = new List<Ranking>();
 
-        //    foreach (var item in Partidas)
-        //    {
+            foreach (var item in Partidas)
+            {
+                if (item.HayCartas(item.jugadores.First(x => x.NumeroJugador == NumJugador.uno), item.jugadores.First(x => x.NumeroJugador == NumJugador.dos)))
+                {
+                    var jugadoralrankig = item.DetectarJugadorPerdedor(item.jugadores.First(x => x.NumeroJugador == NumJugador.uno), item.jugadores.First(x => x.NumeroJugador == NumJugador.dos));
 
-        //    }
-        //}
+                    Jugador agregar = new Jugador();
 
- 
+                    if (jugadoralrankig == item.jugadores.First(x => x.NumeroJugador == NumJugador.uno))
+                    {
+                        agregar = jugadoralrankig;
+                    }
+                    else
+                    {
+                        if (jugadoralrankig == item.jugadores.First(x => x.NumeroJugador == NumJugador.dos))
+                        {
+                            agregar = jugadoralrankig;
+                        }
+                    }
+
+                    Ranking nuevo = new Ranking();
+                    nuevo.Nombre = agregar.nombre;                    
+
+                    if (this.Rankings.Count == 0)
+                    {
+                        nuevo.VecesQueGano = 1;
+                        ListaRetornar.Add(nuevo);                        
+                    }
+                    else
+                    {
+                        bool ok = false;
+                        foreach (var item2 in ListaRetornar)
+                        {
+                            if (item2.Nombre == nuevo.Nombre)
+                            {
+                                item2.VecesQueGano = item2.VecesQueGano + 1;
+                                ok = true;
+                            }
+                        }
+
+                        if (ok == false)
+                        {
+                            nuevo.VecesQueGano = 1;
+                            ListaRetornar.Add(nuevo);                            
+                        }
+                    }
+                }
+            }
+            ListaRetornar.OrderByDescending(x => x.VecesQueGano);
+            this.Rankings = ListaRetornar;
+        }
+
+
     }
 }
